1e60a10dda4fb541994830d1137aa3cd852a64cb
ifalo-ken <ken@ifalo.com>
2020-10-29 14:08:48 +0800

    能夠判斷是否猜對猜錯並且能夠顯示正確訊息

--- js/js.js
+++ js/js.js
@@ -1,22 +1,46 @@
 // 產生一個1到100間的隨機數字。
 var number = Math.floor(Math.random()*100)
-
+console.log(number);
 // 從一開始，紀錄玩家目前回合數。
 var round = 0
 var guesses = []
 function guess() {
     // 獲取 input 內容
     let guessNumber = document.getElementById("number").value
+
     // 清空 input 內容
     document.getElementById("number").value = ""
+
+    // 檢查數字是否猜中。
+    checkingNumber(guessNumber)
+    // 當玩家送出第一個猜測後，將猜測記錄下來，讓玩家可以看到他們之前的猜測。
     // 將陣列顯示在前端
     document.getElementById("guesses").innerText += " " + guessNumber 
 }
+
+function checkingNumber(checkNumber){
+    // console.log(number, checkNumber)
+    // 這裏checkNumber 型別是字串 要轉換成數字
+    checkNumber = parseInt(checkNumber)
+    
+    // 確認猜的數字與亂數產生是否一致
+    if (number === checkNumber) {
+        // 如果數字猜對：
+        // 顯示恭喜訊息。
+        //正確將訊息改成綠色並顯示成功
+        document.querySelector(".alert").style.backgroundColor = 'green'
+        document.querySelector(".alert span").textContent  = "成功"
+    } else {
+        // 錯誤將訊息改成紅色並顯示Wrong
+        document.querySelector(".alert span").textContent  = "失敗"
+    }
+    
+
+}
 // 提供玩家猜數字的方向(太大還是太小)。
-// 當玩家送出第一個猜測後，將猜測記錄下來，讓玩家可以看到他們之前的猜測。
-// 接著檢查數字是否猜中。
-// 如果數字猜對：
-// 顯示恭喜訊息。
+
+
+
 // 使玩家不能再輸入更多猜測(避免把遊戲玩壞)。
 // 顯示控制鈕讓玩家可以重新開始遊戲。
 // 如果數字猜錯而且玩家有剩餘回合數：
